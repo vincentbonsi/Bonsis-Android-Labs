a0388733bd9cf3bd8d57e31147278949f91e912e
bonsi <somebodyvincent20@gmail.com>
2023-11-08 11:14:35 -0500

    Initial commit

---
+++ app/src/main/cpp/bons0017.cpp
@@ -0,0 +1,17 @@
+// Write C++ code here.
+//
+// Do not forget to dynamically load the C++ library into your application.
+//
+// For instance,
+//
+// In MainActivity.java:
+//    static {
+//       System.loadLibrary("bons0017");
+//    }
+//
+// Or, in MainActivity.kt:
+//    companion object {
+//      init {
+//         System.loadLibrary("bons0017")
+//      }
+//    }
