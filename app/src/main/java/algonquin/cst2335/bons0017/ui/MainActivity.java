f0ac0bebbfda405ddace164fa1116dd5cc5fe669
bonsi <somebodyvincent20@gmail.com>
2023-11-08 23:53:35 -0500

    Week 4 commit

--- app/src/main/java/algonquin/cst2335/bons0017/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/bons0017/ui/MainActivity.java
@@ -1,44 +1,74 @@
 package algonquin.cst2335.bons0017.ui;
 
-
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.animation.Animation;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.RotateAnimation;
-import android.widget.ImageView;
-import android.widget.Switch;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import algonquin.cst2335.bons0017.R;
+import algonquin.cst2335.bons0017.ui.SecondActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView imgView;
-    Switch sw;
+    private static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imgView = findViewById(R.id.flagView);
-        sw = findViewById(R.id.switch1);
+        Button loginButton = findViewById(R.id.loginButton);
+        EditText emailEditText = findViewById(R.id.edittextEmail);
 
-        sw.setOnCheckedChangeListener((btn, isChecked) -> {
-            if (isChecked)
-            {
-                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-                rotate.setDuration(5000);
-                rotate.setRepeatCount(Animation.INFINITE);
-                rotate.setInterpolator(new LinearInterpolator());
+        SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
 
-                imgView.startAnimation(rotate);
-            }
-            else {
-                imgView.clearAnimation();
-            }
-        });
+        String emailAddress = prefs.getString("LoginName", "");
+
+        emailEditText.setText(emailAddress);
+        loginButton.setOnClickListener( clk-> {
+            editor.putString("LoginName", emailEditText.getText().toString());
+            editor.apply();
+            Intent nextPage = new Intent( MainActivity.this, SecondActivity.class);
+            nextPage.putExtra( "EmailAddress", emailEditText.getText().toString() );
+            startActivity( nextPage);
+        } );
+
+        Log.w( TAG, "is the first function that gets created when an application is launched. There are several other functions that get called when an application is launching:" );
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w(TAG,"The application no longer responds to user input");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w(TAG,"The application is now responding to user input");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.w(TAG,"The application is now visible on screen");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.w(TAG,"The application is no longer visible");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.w(TAG,"Any memory used by the application is freed");
     }
 }
-
