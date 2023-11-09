ebe8b3ec079d63836b673ba62e2a0775ac8f66ad
bonsi <somebodyvincent20@gmail.com>
2023-11-08 22:03:15 -0500

    Finished Phone Version of Lab 3

--- app/src/main/java/algonquin/cst2335/bons0017/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/bons0017/ui/MainActivity.java
@@ -1,86 +1,45 @@
 package algonquin.cst2335.bons0017.ui;
 
+
+
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.RadioButton;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 import android.widget.Switch;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import algonquin.cst2335.bons0017.R;
-import algonquin.cst2335.bons0017.data.MainViewModel;
-import algonquin.cst2335.bons0017.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    private ActivityMainBinding variableBinding;
-    private MutableLiveData<Boolean> myBooleanVariable;
-    private EditText editText;
-    private Button myButton;
-    private TextView displayTextView;
+
+    ImageView imgView;
+    Switch sw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        MainViewModel model = new ViewModelProvider(this).get(MainViewModel.class);
-        variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(variableBinding.getRoot());
+        imgView = findViewById(R.id.flagView);
+        sw = findViewById(R.id.switch1);
 
-        myBooleanVariable = new MutableLiveData<>();
-        editText = variableBinding.myEditText;
-        myButton = variableBinding.myButton;
-        displayTextView = findViewById(R.id.textView); // Initialize the TextView
+        sw.setOnCheckedChangeListener((btn, isChecked) -> {
+            if (isChecked)
+            {
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
 
-        variableBinding.textView.setText(model.getEditString());
-
-        // Clicking the radio button triggers checking the checkbox and switch
-        RadioButton radioButton = findViewById(R.id.radioButton);
-        radioButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                CheckBox checkBox = findViewById(R.id.checkBox);
-                Switch switchButton = findViewById(R.id.switch1);
-                checkBox.setChecked(isChecked); // Check/uncheck the checkbox
-                switchButton.setChecked(isChecked); // Check/uncheck the switch
+                imgView.startAnimation(rotate);
             }
-        });
-
-        variableBinding.myButton.setOnClickListener(click -> {
-            // Show what was written in the EditText
-            String text = editText.getText().toString();
-            displayTextView.setText("Text entered: " + text); // Update the TextView with entered text
-        });
-
-        myBooleanVariable.observe(this, isChecked -> {
-            Toast.makeText(MainActivity.this, "The value is now: " + isChecked, Toast.LENGTH_SHORT).show();
-        });
-
-        ImageButton my_ImageButton = findViewById(R.id.action_image);
-        variableBinding.actionImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int width = variableBinding.actionImage.getWidth();
-                int height = variableBinding.actionImage.getHeight();
-
-                Toast.makeText(MainActivity.this, "The width = " + width + " and height = " + height, Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        myBooleanVariable.observe(this, new Observer<Boolean>() {
-            @Override
-            public void onChanged(Boolean isChecked) {
-                // Checkbox and switch behavior is handled by the radio button listener
+            else {
+                imgView.clearAnimation();
             }
         });
     }
 }
+
